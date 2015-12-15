637c16d864fbc02c133c09b759b9e42e6131b57d
unknown <jmaldonadom1989@gmail.com>
2015-12-15 14:18:30 -0700

    Added second commit

---
+++ Assets/Scripts/ScreenOrientationC.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenOrientationC : MonoBehaviour {
+
+	// Use this for initialization
+
+		public bool orientationPortrait; 
+		public bool orientationLandscape; 
+		
+	void Start () {
+
+
+
+			
+		Invoke ("screenOrientation",1);
+	
+		 
+	}
+	
+	 
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+
+	}
+
+	void screenOrientation(){
+
+		
+		if(orientationLandscape==true){
+			
+			Screen.orientation = ScreenOrientation.LandscapeRight;
+		}
+		if(orientationPortrait==true){
+			Screen.orientation = ScreenOrientation.Portrait;
+		}
+		if(orientationLandscape==true && orientationPortrait==true){
+			
+
+			Screen.orientation = ScreenOrientation.AutoRotation;
+			Screen.autorotateToLandscapeRight = true;
+			Screen.autorotateToLandscapeLeft = true;
+			Screen.autorotateToPortrait = true;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SimplePlayer : MonoBehaviour {
+	
+	Animator animator;
+	public Text CurrentAnimation;
+	public GameObject AnimationButton;
+
+
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		CurrentAnimation.text = "No Animation";
+
+	
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (this.transform.GetComponent<Renderer> ().isVisible ) {
+			CurrentAnimation.gameObject.transform.parent.gameObject.SetActive (true);
+			AnimationButton.SetActive (true);
+		} else {
+			CurrentAnimation.gameObject.transform.parent.gameObject.SetActive (false);
+			AnimationButton.SetActive (false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+
+		
+
+
+
+			if (animator.GetBool ("Idle1") == true) {
+				animator.SetBool ("Run", true);
+				animator.SetBool ("Idle1", false);
+			} else {
+				animator.SetBool ("Idle1", true);
+				animator.SetBool ("Run", false);
+			}
+			Debug.Log (animator.GetBool ("Idle1"));
+			//Invoke ("ResetTransition",0.01f);
+
+		}
+
+
+
+
+	}
+
+	public void CycleAnimation(){
+
+		Debug.Log ("cycle");
+
+
+
+		if (animator.GetBool ("Idle1") == true) {
+			animator.SetBool ("Run", true);
+			animator.SetBool ("Idle1", false);
+			CurrentAnimation.text="Runing";
+		} else {
+			animator.SetBool ("Idle1", true);
+			animator.SetBool ("Run", false);
+			CurrentAnimation.text="Idle";
+		}
+		Debug.Log (animator.GetBool ("Idle1"));
+		//Invoke ("ResetTransition",0.01f);
+
+	}
+
+}
